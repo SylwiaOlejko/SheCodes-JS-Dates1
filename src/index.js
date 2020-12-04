7e307d7858fad78568e5764ba72bc6f259b288dd
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-04 19:42:09 +0100

    format Date

--- src/index.js
+++ src/index.js
@@ -17,12 +17,44 @@ console.log(currentTime.getFullYear());
 
 console.log(currentTime.getMonth());
 
-//Challenge 6
+//Challenge 6/7
+function Date(date){
 
-let month = now.getMonth();
-let day = now.getDay();
-let year = now.getFullYear();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let currentMonth = months[date.getMonth()];
+let currentDay = days[date.getDay()];
+let currentYear = now.getFullYear();
+let currentDate = date.getDate();
 
 
 
- date = `Today is , ${month}  ${day} ${year}`;
+ let formatDate = `Today is , ${currentDay}  ${currentMonth} ${currentDate}, ${currentYear}`;
+
+ return formatDate;
+}
+
+console.log(Date(currentTime));
